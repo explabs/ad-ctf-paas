282d1376dec51bbbfb9fd99182118eeb55a3f1bb
Ivan Hahanov <ivanh@naliway.local>
2021-10-12 21:18:26 +0300

    update example task

--- terraform/teams.tf
+++ terraform/teams.tf
@@ -1,18 +1,6 @@
 variable "teams" {
-  default = ["naliway", "nakateam"]
+  default = ["naliway"]
 }
 variable "ips" {
-  default = ["10.0.1.10", "10.0.2.10"]
-}
-variable "network_name" {
-  default = ["virtbr-team1", "virtbr-team2"]
-}
-variable "gateways" {
-  default = ["10.0.1.254", "10.0.2.254"]
-}
-variable "macs" {
-  default = [
-    "52:54:00:50:99:c5",
-    "52:54:00:0e:87:be",
-  ]
+  default = ["192.168.122.0"]
 }
