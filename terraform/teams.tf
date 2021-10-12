744bcf3fcbc97539d420cad3cae6f4f6b0b48631
Ivan Hahanov <ivanh@naliway.local>
2021-10-12 21:26:17 +0300

    return teams.tf

--- terraform/teams.tf
+++ terraform/teams.tf
@@ -1,6 +1,19 @@
+
 variable "teams" {
-  default = ["naliway"]
+  default = ["naliway", "nakateam"]
 }
 variable "ips" {
-  default = ["192.168.122.0"]
+  default = ["10.0.1.10", "10.0.2.10"]
+}
+variable "network_name" {
+  default = ["virtbr-team1", "virtbr-team2"]
+}
+variable "gateways" {
+  default = ["10.0.1.254", "10.0.2.254"]
+}
+variable "macs" {
+  default = [
+    "52:54:00:50:99:c5",
+    "52:54:00:0e:87:be",
+  ]
 }
