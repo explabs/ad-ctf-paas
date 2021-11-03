247c957485fd9522c16263e04b1c9c7ba6fc6ad6
Ivan Hahanov <ivanh@naliway.local>
2021-11-03 13:41:59 +0300

    add output

--- terraform/openstack/main.tf
+++ terraform/openstack/main.tf
@@ -16,4 +16,7 @@ resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = openstack_networking_floatingip_v2.fip_1.address
   instance_id = openstack_compute_instance_v2.test.id
 }
+output "ip" {
+  value = openstack_networking_floatingip_v2.fip_1.address
+}
 #openstack server create --nic net-id=b5d27bec-5627-450d-939d-cc319ab92527 --flavor 1 --image e0718133-3b7b-4677-bb21-95188b770716 --key-name test test1
