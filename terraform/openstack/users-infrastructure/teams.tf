529b5b3755fedf556a1b3cfbc056eba48f0bd5bf
Fral738 <frolov1998kc@mail.ru>
2021-11-15 19:00:15 +0300

    add script for downloading files

--- terraform/openstack/users-infrastructure/teams.tf
+++ terraform/openstack/users-infrastructure/teams.tf
@@ -1,10 +1,9 @@
-
 variable "teams" {
-  default = ["naliway", "nakateam"]
+  default = ["naliway", "lap04ka", "1-or-1-1", "rustemo4ka", "khakhanov-inc"]
 }
 variable "ips" {
-  default = ["10.0.1.10", "10.0.2.10"]
+  default = ["10.0.1.10", "10.0.2.10", "10.0.3.10", "10.0.4.10", "10.0.5.10"]
 }
 variable "cidr" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
 }
