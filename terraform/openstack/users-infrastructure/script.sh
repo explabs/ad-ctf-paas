b1fe5f0e7e422bde02dee37df7b307c5f5a917a5
Ivan Hahanov <ivanh@naliway.local>
2021-11-15 21:42:31 +0300

    refactror script.sh

--- terraform/openstack/users-infrastructure/script.sh
+++ terraform/openstack/users-infrastructure/script.sh
@@ -2,33 +2,34 @@
 
 server_ip="$1"
 password="$2"
-if [[ $# -ne 2 ]]; 
-then 
+
+if [[ $# -ne 2 ]];
+then
     echo "illegal number of arguments" >&2
     exit 1
 fi
+
 api_url="http://$server_ip/api/v1"
-TOKEN=$(curl -s -X POST \
- -H 'Accept: application/json' -H 'Content-Type: application/json' \
- --data '{"username":"admin","password":"'"$password"'"}' \
- $api_url/auth/login | jq -r '.token')
+TOKEN=$(curl -s -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' \
+ --data '{"username":"admin","password":"'"$password"'"}' "$api_url"/auth/login | jq -r '.token')
+
 if [[ -z "$TOKEN" ]];
 then
     echo "token is empty" >&2
     exit 1
-else
-    wget -q --header="Authorization: Bearer ${TOKEN}" $api_url/admin/generate/variables -O teams.tf
-    if [[ "$?" != 0 ]]; then
-        echo "Error downloading file"
-        exit 1
-    else
-        echo "teams.tf has been downloaded successfully"
-    fi
-    wget  -qO- --header="Authorization: Bearer ${TOKEN}" $api_url/admin/generate/sshkeys | tar -xz 2> /dev/null
-    if [[ "$?" != 0 ]]; then
-        echo "Error downloading file"
-        exit 1
-    else
-         echo "keys/ directory has been downloaded successfully"
-    fi
 fi
+
+
+function get_data () {
+  http_status=$(curl --write-out "%{http_code}" --silent --output /dev/null -H "Authorization: Bearer ${TOKEN}" "$api_url"/"$1" )
+  if [[ http_status -ne 200 ]]; then
+    echo "http server returns $http_status" >&2
+    exit 1
+  else
+    wget -qO- --header="Authorization: Bearer ${TOKEN}" "$api_url"/"$1"
+  fi
+}
+
+get_data admin/generate/variables > teams.tf
+get_data admin/generate/sshkeys | tar -xz 2> /dev/null
+
